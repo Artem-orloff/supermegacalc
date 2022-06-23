34d317cfb0e94015742359033fa04fddc1255830
Artem-orloff <80464436+Artem-orloff@users.noreply.github.com>
2022-06-23 19:13:29 +0600

    Update Form1.cs

--- calc/calc/Form1.cs
+++ calc/calc/Form1.cs
@@ -12,9 +12,30 @@ namespace calc
 {
     public partial class Form1 : Form
     {
+        public string operation;
+        public string firstArgument, secondArgument;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_click(object sender, EventArgs e)
+        {
+            Button A = (Button)sender;
+            operation = A.Text;
+            firstArgument = textBox10.Text;
+            secondArgument = textBox11.Text;
+
+            firstNum = Convert.ToDouble(firstArgument);
+            secondNum = Convert.ToDouble(secondArgument);
         }
     }
 }
