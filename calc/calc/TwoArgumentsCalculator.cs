9bc3e73fa9a2cea073ff8b3b7586cc21488491e2
Matthew <94904232+kquezel@users.noreply.github.com>
2022-06-23 19:08:01 +0600

    Create TwoArgumentsCalculator.cs

---
+++ calc/calc/TwoArgumentsCalculator.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace calc
+{
+    static class TwoArgumentsCalculator
+    {
+        public static double Creatror(string calculatorName)
+
+        {
+        }
+    }
+}
